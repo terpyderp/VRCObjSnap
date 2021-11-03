27aaf03cbebaa27954f0a96a583a3a38cd50683a
terpyderp <terpyderp2@gmail.com>
2021-11-03 18:13:42 -0500

    Added the actual stuff

---
+++ snapObject.cs
@@ -0,0 +1,71 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class snapObject : UdonSharpBehaviour {
+	Collider collider;
+	snapPoint[] points;
+	snapPoint point;
+	Transform OGParent;
+
+	void Start() {
+		collider = GetComponent<Collider>();
+		OGParent = transform.parent;
+		points = new snapPoint[0];
+	}
+
+	void OnTriggerEnter(Collider otherCollider) {
+		snapPoint temp = otherCollider.gameObject.GetComponent<snapPoint>();
+		if (temp != null) {
+			// add new snapPoint to points
+			snapPoint[] pointsTemp = new snapPoint[points.Length+1];
+			for (int i = 0; i < points.Length; i++) {
+				pointsTemp[i] = points[i];
+			}
+			pointsTemp[pointsTemp.Length-1] = temp;
+			points = pointsTemp;
+			point = temp;
+		}
+	}
+
+	void OnTriggerExit(Collider otherCollider) {
+		if (points.Length > 0) {
+			// detect which collider needs to be removed from the points var
+			int indexToDel = 0;
+			for (int i = 0; i < points.Length; i++) {
+				if (otherCollider.gameObject.GetInstanceID() == points[i].gameObject.GetInstanceID()) {
+					indexToDel = i;
+					break;
+				}
+			}
+
+			// remove the collider from the points var
+			snapPoint[] pointsTemp = new snapPoint[points.Length-1];
+			int ii = 0;
+			for (int i = 0; i < points.Length; i++) {
+				if (i != indexToDel) {
+					pointsTemp[ii] = points[i];
+					ii++;
+				}
+			}
+			points = pointsTemp;
+			if (points.Length > 0) {
+				point = points[points.Length-1];
+			} else {
+				point = null;
+			}
+		}
+	}
+
+	void OnDrop() {
+		if (point == null) {
+			transform.parent = OGParent;
+		} else {
+			transform.position = point.getSnapPos(transform.position);
+			// transform.rotation = point.getSnapRot(transform);
+			transform.rotation = point.transform.rotation;
+			transform.parent = point.transform;
+		}
+	}
+}
---
+++ snapPoint.cs
@@ -0,0 +1,33 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class snapPoint : UdonSharpBehaviour {
+	public bool isSurface;
+	// public uint rotationSnap;
+
+	public Vector3 getSnapPos(Vector3 pos) {
+		if (isSurface) {
+			pos.y = transform.position.y;
+			return pos;
+		} else {
+			return transform.position;
+		}
+	}
+
+	public Quaternion getSnapRot(Transform trans) {
+		// rot.x = 0;
+		// rot.z = 0;
+		// if (rotationSnap > 0) {
+		// 	// float rotationSnapRad = 180 * rotationSnap;
+		// 	// // Debug.Log((rot.y/rotationSnapRad) + 0.5);
+		// 	Debug.Log(rot.y);
+		// 	// rot.y = (float) ((int) ((rot.y/rotationSnapRad) + 0.5)) * rotationSnapRad;
+
+		// 	rot.y = (float) ((int) (rot.y*2))/2;
+		// }
+
+		return Quaternion.FromToRotation(trans.up, transform.up);
+	}
+}
